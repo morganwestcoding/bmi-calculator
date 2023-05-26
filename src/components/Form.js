5834a11fb2376dcb6bec82628ef10c5f246046c2
Morgan West <mewwes@gmail.com>
2023-05-25 18:52:01 -0700

    run correct

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,7 +4,28 @@ import Button from './Button.js';
 import '../styles/Form.css';
 
 class Form extends React.Component {
-    
+    constructor(props) {
+        super(props);
+        this.state = {
+            height: '',
+            weight: '',
+            bmi: '',
+            bmiClass: ''
+        };
+
+
+        this.heightChange = this.heightChange.bind(this);
+        this.weightChange = this.weightChange.bind(this);
+    }
+
+    weightChange(weightValue) {
+    this.setState({ weight : weightValue });
+    }
+
+    heightChange(heightValue) {
+    this.setState({ height:  heightValue });
+    }
+
     computeBmi = () => {
         let bmiValue = (this.state.weight / this.state.height) / this.state.height;
         this.setState({ bmi: bmiValue });
@@ -12,42 +33,42 @@ class Form extends React.Component {
         this.setState({ bmiClass: bmiClass });
     }
 
-    getBmi(bmi) {
-    if(bmi < 18.5) {
+        getBmi = (bmiValue) => {
+        if(bmiValue < 18.5) {
         return "Underweight";
     }
-    if(bmi >= 18.5 && bmi < 24.9) {
+    if(bmiValue >= 18.5 && bmi < 24.9) {
         return "Normal weight";
     }
-    if(bmi >= 25 && bmi < 29.9) {
+    if(bmiValue >= 25 && bmi < 29.9) {
         return "Overweight";
     }
-    if(bmi >= 30) {
+    if(bmiValue >= 30) {
         return "Obesity";
     }
+
 }
 
     render() {
-        return(
-            
-         <div>
-            <div className="row">
-                <TextInput label="Height" placeholder="Enter height in meters" onChange={this.heightChange} />
+        return (
+            <div>
+                <div className="row">
+                    <TextInput label="Height" placeholder="Enter height in meters" onChange={this.heightChange} />
+                </div>
+                <div className="row">
+                    <TextInput label="Weight" placeholder="Enter weight in kg" onChange={this.weightChange} />
+                </div>
+                <div className="row">
+                    <Button label="SUBMIT" onClick={this.computeBmi} />
+                </div>
+                <div className="row">
+                    <h3>BMI = {this.state.bmi}</h3>
+                </div>
+                    <div className="row">
+                    <h3>{this.state.bmiClass}</h3>
+                    </div>
             </div>
-            <div className="row">
-                <TextInput label="Weight" placeholder="Enter weight in kg" onChange={this.weightChange} />
-            </div>
-                        <div className="row">
-                <Button label="SUBMIT" onClick={ this.computeBmi } />
-            </div>
-            <div className="row">
-            <h3>BMI = {this.state.bmi}</h3>
-            </div>
-            <div className="row">
-            <h3>{this.state.bmiClass}</h3>
-            </div>
-        </div>
-        )
+        );
     }
 }
 
